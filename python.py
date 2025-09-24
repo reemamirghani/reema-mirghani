c4d282a506a6f0d2176bd416d74c7561f90a002d
reemamirghani <rbm2793@g.rit.edu>
2025-09-24 11:26:41 +0400

    hi

---
+++ python.py
@@ -0,0 +1,27 @@
+def even_odd(x):
+    if x%2==0:
+        print("even")
+    else:
+        print("odd")
+def square_number(x):
+    square=x*x
+    print(square)
+def postive_negative(x):
+    if x>0:
+        print("postive")
+    elif x<0:
+        print("negative")
+def square_number(x):
+    square=x*x
+    print(square)
+def cube_number(x):
+    cube=x*x*x
+    print(cube)
+def main():
+    x=int(input("add a number"))
+    print(even_odd(x))
+    print(cube_number(x))
+    print(square_number(x))
+    print(postive_negative(x))
+
+main()
